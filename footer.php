59b9305f7a9668ede45ac72cdc00963103b27755
sebaInfanta <sebainfanta@gmail.com>
2016-04-06 03:49:16 -0300

    Se integró mini-footer con contenido simulado

--- footer.php
+++ footer.php
@@ -13,13 +13,29 @@
 
 		</div><!-- #content -->
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mdlpress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mdlpress' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
+		<footer id="colophon" class="mdl-mini-footer" role="contentinfo">
+			
+			<div class="mdl-mini-footer__left-section">
+				<div class="mdl-logo">
+					<?php bloginfo('name'); ?>
+				</div>
+				<ul class="mdl-mini-footer__link-list">
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 2</a></li>
+					<li><a href="">Link 3</a></li>
+				</ul>				
+			</div>
+ 
+			<div class="site-info mdl-mini-footer__right-section">
 				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mdlpress' ), 'mdlpress', '<a href="http://trevo.cl" rel="designer">Sebastián Infanta</a>' ); ?>
+				<button class="mdl-mini-footer__social-btn"><i class="material-icons">queue_music</i></button>
+				<button class="mdl-mini-footer__social-btn"><i class="material-icons">android</i></button>
+				<button class="mdl-mini-footer__social-btn"><i class="material-icons">attachment</i></button>
+
 			</div><!-- .site-info -->
+
 		</footer><!-- #colophon -->
+
 	</main>
 </div><!-- #page -->
 
